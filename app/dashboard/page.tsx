5164ac2791b3f3e284798af644097c12e4ba9f3b
SinghAman21 <singhaman1825@gmail.com>
2025-11-15 15:36:54 +0530

    shadcn + tw + feat routes + dark mode + pnpm

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,3 @@
-import { ModeToggle } from "@/components/modetoggle";
 import Image from "next/image";
 
 export default function Home() {
@@ -13,8 +12,6 @@ export default function Home() {
           height={20}
           priority
         />
-        Toggle Theme here: 
-        <ModeToggle/>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
